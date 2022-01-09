dc4a2fec6ac81f41dac4e4c0fd3112a70f486e9b
Mohammed Naji <m.naji1512@gmail.com>
2022-01-09 18:47:21 +0300

    lec13

---
+++ database/migrations/2022_01_09_151844_create_blog_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog', function (Blueprint $table) {
+            $table->id(); // id
+            $table->string('title', 150)->nullable();
+            $table->text('content')->default('No Content Yet');
+            $table->timestamps(); // updated_at, created_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog');
+    }
+}
